e0d03a5b70eb309345ff0e2d591a1b2b236428bf
Haseeb <haseebbhatti742@gmail.com>
2024-03-14 02:47:30 +0500

    new code added with updated

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from flask import Flask, request, render_template, url_for, redirect
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-
-@app.route('/basket')
-def basket():
-    return render_template('basket.html')
+basket_cart = list()
+# @app.route('/basket')
+# def basket():
+#     return render_template('basket.html')
 
 @app.route('/frontpage')
 def frontpage():
@@ -16,7 +16,6 @@ def frontpage():
     cursor.execute("SELECT name, rating, image_url FROM restaurants")
     restaurants = cursor.fetchall()
     connect.close()
-    print(restaurants)
     return render_template('frontpage.html', restaurants=restaurants)
 
 
@@ -97,6 +96,63 @@ def show_menu(restaurant: str):
 
     # print(data)
     return render_template('menu.html', restaurant_menu=data, restaurant=restaurant)
+
+
+@app.route('/add-to-basket', methods=['POST','GET'])
+def add_to_basket():
+    global basket_cart
+    if request.method == 'GET':
+        return render_template('basket.html',basket_cart_data=basket_cart)
+    if request.method == 'POST':
+        form_data = request.form.to_dict()
+        basket_cart.append({
+            'item_name' : form_data.get('item_name'),
+            'item_price' : form_data.get('item_price')
+        })
+        return redirect(url_for('show_menu', restaurant=form_data.get('restaurant_name')))
+
+@app.route('/remove_from_basket', methods=['POST'])
+def remove_from_basket():
+    if request.method == 'POST':
+        item_index = int(request.form['item_index'])
+        del basket_cart[item_index]
+        return redirect(url_for('show_basket'))
+
+@app.route('/basket')
+def show_basket():
+    global basket_cart
+    total_price = 0
+    for i in basket_cart:
+        total_price+=float(i['item_price'].replace('£',''))
+    total_price = "{:.2f}".format(total_price)
+    return render_template('basket.html', basket_cart_data=basket_cart, total_price=total_price)
+
+@app.route('/checkout',  methods=['POST'])
+def checkout_cal():
+    global basket_cart
+    total_price = 0
+    for i in basket_cart:
+        total_price+=float(i['item_price'].replace('£',''))
+
+    total_price = "{:.2f}".format(total_price)
+    total_price = float(total_price)
+    if request.method == 'POST':
+        tip = request.form['tip']
+        print('check out this tip:',tip,type(tip))
+        if tip in ['not_now','10%','15%']:
+            if tip == 'not_now':
+                tip = 0.0
+            elif tip == '10%':
+                tip = total_price * 0.10
+            elif tip == '15%':
+                tip = total_price * 0.10
+        else:
+            tip = float(tip)
+
+        
+        total_price_with_tip = tip + float(total_price)
+        total_price_with_tip = "{:.2f}".format(total_price_with_tip)
+        return render_template('checkout.html',item_price = total_price, tip = tip, total_price=total_price_with_tip, basket_cart_data=basket_cart)
 
 
 if __name__ == '__main__':
